f86493f23ddaf4a212d836b5ee314265c55e8e6c
Jack Arthur Harrhy <me@jackharrhy.com>
2020-09-16 22:40:30 -0230

    lol

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 rm -rf ./build
 mkdir -p ./build
-crystal run generate-site.cr > ./build/index.html
+crystal run generate-site.cr -- --output-html > ./build/index.html
 cp -r ./images ./build/images
